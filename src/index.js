e1de194abaa0636c5bb28675e5029f948c0a3632
dwyfrequency <jdwy215@gmail.com>
2019-06-25 15:27:06 -0400

    adding initial schema, resolvers and server def

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-// 1
+/* 1
+ * The typeDefs constant defines your GraphQL schema. Here, it defines a simple Query type with one field called info.
+ * This field has the type String!. The exclamation mark in the type definition means that this field can never be null.
+ */
 const typeDefs = `
 type Query {
   info: String!
 }
 `;
 
-// 2
+/* 2
+ * The resolvers object is the actual implementation of the GraphQL schema. Notice how its structure is identical to the structure of the type definition inside typeDefs: Query.info.
+ *  */
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
   },
 };
 
-// 3
+/*
+ * Finally, the schema and resolvers are bundled and passed to the GraphQLServer which is imported from graphql-yoga. This tells the server what API operations are accepted and how they should be resolved.
+ */
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
