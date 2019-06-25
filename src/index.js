55c31927de3450c7c862e1b5dd1fcc0327368e19
dwyfrequency <jdwy215@gmail.com>
2019-06-25 15:49:58 -0400

    added gql schema file and mutation def

--- src/index.js
+++ src/index.js
@@ -4,18 +4,37 @@ const { GraphQLServer } = require('graphql-yoga');
  * The typeDefs constant defines your GraphQL schema. Here, it defines a simple Query type with one field called info.
  * This field has the type String!. The exclamation mark in the type definition means that this field can never be null.
  */
-const typeDefs = `
-type Query {
-  info: String!
-}
-`;
 
 /* 2
  * The resolvers object is the actual implementation of the GraphQL schema. Notice how its structure is identical to the structure of the type definition inside typeDefs: Query.info.
  *  */
+// 1
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
   },
 };
 
@@ -23,7 +42,7 @@ const resolvers = {
  * Finally, the schema and resolvers are bundled and passed to the GraphQLServer which is imported from graphql-yoga. This tells the server what API operations are accepted and how they should be resolved.
  */
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
